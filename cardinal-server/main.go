42c693dd682c1cb80146e602fa77dac63fe5f0e4
Klithik <klithik@proton.me>
2024-09-13 18:49:23 -0300

    added function to create, encrypt and compare password

--- cardinal-server/main.go
+++ cardinal-server/main.go
@@ -4,10 +4,42 @@ import (
     "fmt"
     "log"
     "net/http"
+    "golang.org/x/crypto/bcrypt"
+    "time"
+    "math/rand"
 )
+
+func generateRandomString(length int) string {
+    const charset = "abcdefghijklmnopqrstuvwxyz0123456789"
+	seed := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(seed)
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[r.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func encryptPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
+
+func comparePassword(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
 
 func main(){
     mux := http.NewServeMux()
+    pass := generateRandomString(rand.Intn(5) + 12)
+    fmt.Println("App password: " + pass)
+    pass,err := encryptPassword(pass)
+    if err != nil {
+        panic(err)
+    }
+    log.Println("Encrypted password: " + pass)
     log.Print("Listening...")
 
     go mux.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request){
